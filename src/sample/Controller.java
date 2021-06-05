ad13850d2a0fcce9993a4446f727514ebfcf9487
Ryan <ryanbcwang@gmail.com>
2021-06-04 20:50:30 -0700

    Icon, info for icon and GH

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,55 @@
 package sample;
 
+import com.sun.deploy.uitoolkit.impl.fx.HostServicesFactory;
+import com.sun.javafx.application.HostServicesDelegate;
+import javafx.application.HostServices;
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+
+import javax.print.DocFlavor;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Controller {
+
+    public MenuItem githubMI;
+    public MenuItem iconMI;
+
+    public TextArea areaText;
+
+    public void onSave() {
+
+    }
+
+    public void onLoad() {
+
+    }
+
+    public void onClose() {
+        System.exit(0);
+    }
+
+
+    public void onInfoGithub(ActionEvent actionEvent) {
+        try {
+            Desktop.getDesktop().browse(new URL("https://github.com/Ry4nW/Text-Editor").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onInfoIcon(ActionEvent actionEvent) {
+        try {
+            Desktop.getDesktop().browse(new URL("https://www.flaticon.com/free-icon/text-editor_196308").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,15 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.getIcons().add(new Image("https://image.flaticon.com/icons/png/512/196/196308.png"));
         primaryStage.setTitle("Text Editor");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
     }
 
 
